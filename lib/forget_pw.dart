8828e89cceaa4c8ac7d0349bfe463c7a549a8c51
Jihan-Almak <jalmakrami@gmail.com>
2022-12-14 19:18:43 +0300

    forget password v2

--- lib/forget_pw.dart
+++ lib/forget_pw.dart
@@ -2,16 +2,75 @@
 
 import 'package:flutter/material.dart';
 
-class Forgot_pw extends StatefulWidget {
-  const Forgot_pw({super.key});
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
 
   @override
-  State<Forgot_pw> createState() => _Forgot_pwState();
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
 }
 
-class _Forgot_pwState extends State<Forgot_pw> {
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+
+
+  @override 
+  void dispose(){
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {}
+   
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Text(
+            'Enter Your Email and we will send you a password reset link',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+            ),
+        ),
+
+        const SizedBox(height: 10,),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              hintText: 'Email' ,
+              fillColor: Colors.grey[200],
+              filled: true,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10,),
+
+          MaterialButton(
+            onPressed: passwordReset,
+            color: Colors.blue,
+            child: const Text('Reset Password'),
+            ),
+      ],
+      ),
+    );
   }
 }
+
+
+
