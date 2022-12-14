994231e2b2a584160291f85872be56456a0bbba5
Jihan-Almak <jalmakrami@gmail.com>
2022-12-14 19:11:19 +0300

    forget password v1

---
+++ lib/forget_pw.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Forgot_pw extends StatefulWidget {
+  const Forgot_pw({super.key});
+
+  @override
+  State<Forgot_pw> createState() => _Forgot_pwState();
+}
+
+class _Forgot_pwState extends State<Forgot_pw> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
