dc37616061727b55838a16d3ca08560260d13599
reem1alshareef <441200087@student.ksu.edu.sa>
2022-12-23 01:00:23 +0300

    .
    .

---
+++ lib/screens/logOutAdmin.dart
@@ -0,0 +1,36 @@
+import 'dart:js';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+//import 'WelcomeScreen.dart'; بتول سوي الامبورت
+
+class logOutAdmin extends StatelessWidget{
+  static const String screenRoute = 'logOutAdmin';
+  logOutAdmin({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+  
+void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  //Navigator.pushNamed(context, WelcomeScreen.screenRoute); بتول ضبطي النافيقيتر 
+  }
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      actions: [
+        IconButton(onPressed: signUserOut,
+        icon: Icon(Icons.logout),
+        )
+      ],
+      ),
+      body: Center (
+        child: Text(
+          "you have logged in As" + user.email!,
+          style: TextStyle(fontSize: 20),
+        )),
+    );
+    
+  }
+}
+
