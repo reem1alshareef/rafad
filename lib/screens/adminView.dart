b50124770593b7208e63269166d0ecfb5cde8a90
sadeem74 <109709369+sadeem74@users.noreply.github.com>
2023-01-06 18:54:41 +0300

    Update adminView.dart

--- lib/screens/adminView.dart
+++ lib/screens/adminView.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/src/widgets/editable_text.dart';
-//import 'package:rafad1/screens/logOutAdmin.dart';
+import 'package:rafad1/screens/logOutAdmin.dart';
 import 'package:rafad1/screens/welcome_screen.dart';
 
 
@@ -269,6 +269,12 @@ class _ViewState extends State<View> {
                                                                     Color(
                                                                         0xFF455D83)),
                                                             onPressed: () {
+
+
+
+
+
+                                                              
                                                               Navigator.of(
                                                                       context)
                                                                   .pop();
@@ -278,14 +284,17 @@ class _ViewState extends State<View> {
                                                                     .instance
                                                                     .collection(
                                                                         'Campaign-Account')
-                                                                    .doc(storedocs[i]['UID'])
+                                                                    .doc(storedocs[
+                                                                            i]
+                                                                        ['UID'])
                                                                     .delete(); // to remove it from UI
                                                                     _firestore
                                                   .collection(
                                                       'AcceptedCampaigns')
                                                   .doc(storedocs[i]['UID'])
-                                                  .set({ 
-                                                    'UID': storedocs[i]['UID'],// To add it to the accepted collection
+                                                  .set({ // To add it to the accepted collection
+                                                'UID': storedocs[i]['UID'],
+                                                'description' : '',
                                                 'status': 'accepted',
                                                 'name': storedocs[i]
                                                     ['nameCampaign'],
@@ -395,8 +404,9 @@ class _ViewState extends State<View> {
                                                                       'RejectedCampaigns')
                                                                   .doc(storedocs[
                                                                       i]['UID'])
-                                                                  .set({ 
-                                                                     'UID': storedocs[i]['UID'],// to add it to rejected collection
+                                                                  .set({ // to add it to rejected collection
+                                                                     'UID': storedocs[i]['UID'],
+                                                                     'description' : '',
                                                                 'name': storedocs[
                                                                         i][
                                                                     'nameCampaign'],
