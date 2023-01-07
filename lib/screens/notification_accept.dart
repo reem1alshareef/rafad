426901a44062bddb1cfb0d51655afcccd745ae50
reem1alshareef <441200087@student.ksu.edu.sa>
2023-01-07 18:24:48 +0300

    /

--- lib/screens/notification_accept.dart
+++ lib/screens/notification_accept.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:math';
 
