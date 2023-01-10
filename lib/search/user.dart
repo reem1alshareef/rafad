fd11425eedb8310eabe2fadc225006afd75d4f3e
Jihan-Almak <jalmakrami@gmail.com>
2023-01-10 20:35:42 +0300

    lllll

--- lib/search/user.dart
+++ lib/search/user.dart
@@ -1,21 +1,35 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class Users{
   String? email;
   String? name;
-  String? id;
+  String? pilgrimID;
+  String? pharma;
+  String? disease;
+  String? hajId;
+  String? phone;
 
 Users({
   this.email,
   this.name,
-  this.id,
+  this.pilgrimID,
+  this.pharma,
+  this.disease,
+  this.hajId,
+  this.phone,
 });
 
 Users.fromJson(Map <String, dynamic> json){
 email = json['email'];
 name = json['name'];
-id = json['id'];
+pilgrimID = json['pilgrimID'];
+pharma = json['pharma'];
+disease = json['disease'];
+hajId = json['hajId'];
+phone = json['phone'];
 }
 
 Map<String, dynamic> toJson(){
@@ -23,7 +37,11 @@ Map<String, dynamic> toJson(){
   final Map<String, dynamic> data = Map<String, dynamic>();
   data['email'] = email;
   data['name'] = name;
-  data['id'] = id;
+  data['pilgrimID'] = pilgrimID;
+  data['pharma'] = pharma;
+  data['disease'] = disease;
+  data['hajId'] = hajId;
+  data['phone'] = phone;
 
 return data;
 }
