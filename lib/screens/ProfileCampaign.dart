0c812bf5325b5e7668484331364cc04998bd48a4
reem1alshareef <441200087@student.ksu.edu.sa>
2023-01-23 14:12:11 +0300

    .

--- lib/screens/ProfileCampaign.dart
+++ lib/screens/ProfileCampaign.dart
@@ -42,63 +42,63 @@ class _logOutCampaign extends State<ProfileCampaign> {
   double _latitude = 0;
   double _longitude = 0;
   
-/*
-class Location extends StatefulWidget {
-  static const String screenRoute = 'Location.dart';
-  const Location({super.key});
 
-  @override
-  _emergencyState createState() => _emergencyState();
-}
+// class Location extends StatefulWidget {
+//   static const String screenRoute = 'Location.dart';
+//   const Location({super.key});
 
-class _emergencyState extends State<Location> {
+//   @override
+//   _emergencyState createState() => _emergencyState();
+// }
+
+// class _emergencyState extends State<Location> {
   
-  final _firestore = FirebaseFirestore.instance;
+//   final _firestore = FirebaseFirestore.instance;
 
-  final _controller = TextEditingController();
+//   final _controller = TextEditingController();
   
-  double _latitude = 0;
-  double _longitude = 0;
+//   double _latitude = 0;
+//   double _longitude = 0;
   
-  @override 
-  void initState(){
-    _emergencyState();
-    print(FirebaseAuth.instance.currentUser?.uid);
-    super.initState();
-  }
+//   @override 
+//   void initState(){
+//     _emergencyState();
+//     print(FirebaseAuth.instance.currentUser?.uid);
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Update campaign location'),
-        backgroundColor:  Color.fromARGB(255, 6, 78, 246),
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.pushNamed(context, WelcomeScreen.screenRoute);
-            },
-          ),
+//     return Scaffold(
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false,
+//         title: Text('Update campaign location'),
+//         backgroundColor:  Color.fromARGB(255, 6, 78, 246),
+//         elevation: 0,
+//         actions: <Widget>[
+//           IconButton(
+//             icon: Icon(
+//               Icons.logout,
+//               color: Colors.white,
+//             ),
+//             onPressed: () {
+//               FirebaseAuth.instance.signOut();
+//               Navigator.pushNamed(context, WelcomeScreen.screenRoute);
+//             },
+//           ),
 
-Container(
-              //كود الخلفيه
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assests/images/background.png"),
-                    fit: BoxFit.cover),
-              ),
-            ), // كود الخلفيه لين هنا 
+// Container(
+//             //كود الخلفيه
+//            decoration: BoxDecoration(
+//                 image: DecorationImage(
+//                     image: AssetImage("assests/images/background.png"),
+//                     fit: BoxFit.cover),
+//               ),
+//             ), // كود الخلفيه لين هنا 
 
-        ],
-      */
+        //],
+      
 
 
   @override
