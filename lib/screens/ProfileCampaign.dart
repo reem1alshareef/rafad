7dbca19fbdb4fd26cdbfbf3ec05409a43575d245
reem1alshareef <441200087@student.ksu.edu.sa>
2023-01-23 14:27:57 +0300

    .

--- lib/screens/ProfileCampaign.dart
+++ lib/screens/ProfileCampaign.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rafad1/screens/forget_pw.dart';
 import 'package:rafad1/screens/welcome_screen.dart';
 import 'package:rafad1/widgets/my_button.dart';
-// try 
+
 import 'add_description.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,84 +19,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/src/widgets/editable_text.dart';
 import 'package:rafad1/screens/welcome_screen.dart';
 
-import 'package:rafad1/screens/mapService.dart';
+import 'package:rafad1/screens/LocationService.dart';
 import '../widgets/my_button.dart';
 
+import 'package:rafad1/screens/emergencyList.dart';
+
+import 'mapService.dart';
+
+
 class ProfileCampaign extends StatefulWidget {
-  static const String screenRoute = 'logOutCampaign';
+  static const String screenRoute = 'ProfileCampaign.dart';
   const ProfileCampaign({super.key});
 
   @override
-  State<ProfileCampaign> createState() => _logOutCampaign();
+  _ProfileCampaignState createState() => _ProfileCampaignState();
 }
-
-class _logOutCampaign extends State<ProfileCampaign> {
-  final user = FirebaseAuth.instance.currentUser!;
+class _ProfileCampaignState extends State<ProfileCampaign> {
   final _firestore = FirebaseFirestore.instance;
 
   final _controller = TextEditingController();
+  late final  double _latitude;
+  late final double  _longitude;
 
-  double _latitude = 0;
-  double _longitude = 0;
-
-/*
-class Location extends StatefulWidget {
-  static const String screenRoute = 'Location.dart';
-  const Location({super.key});
-
-  @override
-  _emergencyState createState() => _emergencyState();
-}
-
-class _emergencyState extends State<Location> {
   
-  final _firestore = FirebaseFirestore.instance;
-
-  final _controller = TextEditingController();
-  
-  double _latitude = 0;
-  double _longitude = 0;
-  
-  @override 
-  void initState(){
-    _emergencyState();
-    print(FirebaseAuth.instance.currentUser?.uid);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Update campaign location'),
-        backgroundColor:  Color.fromARGB(255, 6, 78, 246),
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.pushNamed(context, WelcomeScreen.screenRoute);
-            },
-          ),
-
-Container(
-              //كود الخلفيه
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assests/images/background.png"),
-                    fit: BoxFit.cover),
-              ),
-            ), // كود الخلفيه لين هنا 
-
-        ],
-      */
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,34 +59,30 @@ Container(
         backgroundColor: const Color(0xFF455D83),
         elevation: 0,
       ),
-      body: Center(
-        child: Column(
+     body:Center(
+      child: Column(
           children: [
+
             MyButton(
               color: Color.fromARGB(55, 4, 66, 55),
               title: 'click here to udpate your Location',
-              onPressed: () async {
+                onPressed: () async{
 //////////////////////////الكود اللي بيرسل اللوكيشن الحالي لليوزر
 
-                mapService _locationService = mapService();
-                _locationService.sendLocationToDataBase(context);
-
+                  mapService _mapService = mapService();
+                  _mapService.sendLocationToDataBase(context);
+                  
 ///////////////////////////////
-              },
+                 
+                },
             ),
-            MyButton(
-              color: Color.fromARGB(55, 4, 66, 55),
-              title: 'click here to see Location',
-              onPressed: () async {
-                mapService _locationService = mapService();
-                _locationService.goToMaps(24.7231819, 46.6367413);
+            
 
-///////////////////////////////
-              },
+
+      ],
             ),
-          ],
-        ),
-      ),
+
+    ),
     );
-  } //throw UnimplementedError();
+    }//throw UnimplementedError();
 }
