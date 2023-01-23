4c7287b2b4d8ec75ca9497764d8b1d123d709806
reem1alshareef <441200087@student.ksu.edu.sa>
2023-01-23 14:04:14 +0300

    .

--- lib/screens/.env.dart
+++ lib/screens/.env.dart
@@ -1,1 +1,1 @@
-const String googleAPIKey = 'AIzaSyAFI9ICH58se0xSvDuV9BBaa_ZkrX6jUys';
+//const String googleAPIKey = 'AIzaSyAFI9ICH58se0xSvDuV9BBaa_ZkrX6jUys';
---
+++ lib/screens/emergencyList.dart
@@ -0,0 +1,258 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rafad1/screens/welcome_screen.dart';
+
+import 'package:rafad1/screens/LocationService.dart';
+import 'package:rafad1/screens/emergency.dart';
+import 'package:rafad1/screens/LocationService.dart';
+
+
+class emergencyList extends StatefulWidget {
+  static const String screenRoute = 'emergencyList.dart';
+  const emergencyList({super.key});
+
+  @override
+  _emergencyListState createState() => _emergencyListState();
+}
+class _emergencyListState extends State<emergencyList> {
+  final _firestore = FirebaseFirestore.instance;
+
+  final _controller = TextEditingController();
+  late final  double _latitude;
+  late final double  _longitude;
+
+
+  //  DocumentSnapshot variable = await _firestore
+  //  .collection('Pilgrims-Account')
+  //   .doc(storedocs[i]['UID']).get(
+  //    {
+  //   double latitude = variable['latitude'];
+  //   double  longitude = variable['longitude'];
+  //   },
+  //   );
+  
+
+  
+  @override
+void DataStreams() async {
+  DocumentSnapshot variable = await _firestore
+                                    .collection('AcceptedCampaigns')
+                                    .doc(FirebaseAuth.instance.currentUser?.uid)
+                                    .get();
+                                String pilgrimID = variable['pilgrimID'];
+    await for (var snapshot
+        in _firestore.collection('pilgrimEmrgency').snapshots()) {
+      
+      for (var pilgrim in snapshot.docs) {
+        print(pilgrim.data());
+      }
+    }
+  }
+
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> dataStream = FirebaseFirestore.instance
+        .collection('AcceptedCampaigns')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('pilgrimEmrgency')
+        .snapshots();
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('list of requesting pilgrims emergency'),
+        backgroundColor:  Color.fromARGB(255, 184, 20, 20),
+        elevation: 0,
+      ),
+
+            body: SingleChildScrollView(
+            child: Column(children: [
+              
+      Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+          child: StreamBuilder<QuerySnapshot>(
+              stream: dataStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  // TODO : add snackbar
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                final List storedocs = [];
+                snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map a = document.data() as Map<String, dynamic>;
+                  storedocs.add(a);
+                  a['id'] = document.id;
+                }).toList();
+                return Column(
+                    children: List.generate(
+                        storedocs.length,
+                        (i) => SingleChildScrollView(
+                              child: ExpansionTileCard(
+                                elevation: 2,
+                                initialPadding:
+                                    EdgeInsets.only(bottom: 7, top: 7),
+                                baseColor: Colors.blueGrey[50],
+                                expandedColor: Colors.blueGrey[50],
+                                //key: cardA,
+                                leading: CircleAvatar(
+                                  child: Image.asset("assests/images/emergrncyIcon.png"),
+                                  backgroundColor: Color(0xFF788AA4),
+                                ),
+                                title: Text(
+                                  storedocs[i]['name'],
+                                ),
+                                subtitle: Text(
+                                  "information of Pilgrim who need emergency",
+                                  style: TextStyle(
+                                      fontSize: 11, color: Color.fromARGB(255, 184, 20, 20)),
+                                ),
+                                children: [
+                                  Divider(
+                                    thickness: 1.7,
+                                    height: 1.0,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 16.0,
+                                        vertical: 10.0,
+                                      ),
+                                      child: Center(
+                                        child: Column(children: [
+                                          Column(children: [
+                                            
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Column(children: [
+                                                Text(
+                                                  'Pilgrim\'s Phone Number:  ',
+                                                  style: TextStyle(
+                                                      color: Color(0xFF455D83),
+                                                      fontWeight:
+                                                          FontWeight.w500),
+                                                ),
+                                                Text(
+                                                  storedocs[i]['number'],
+                                                  style:
+                                                      TextStyle(fontSize: 12),
+                                                ),
+                                              ]),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Column(children: [
+                                                Text(
+                                                  'Pilgrim\'s Diseases:  ',
+                                                  style: TextStyle(
+                                                      color: Color(0xFF455D83),
+                                                      fontWeight:
+                                                          FontWeight.w500),
+                                                ),
+                                                Text(
+                                                  storedocs[i]['disease'],
+                                                  style:
+                                                      TextStyle(fontSize: 12),
+                                                )
+                                              ]),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Column(children: [
+                                                Text(
+                                                  'Pilgrim\'s pharma:  ',
+                                                  style: TextStyle(
+                                                      color: Color(0xFF455D83),
+                                                      fontWeight:
+                                                          FontWeight.w500),
+                                                ),
+                                                Text(
+                                                  storedocs[i]['pharma'],
+                                                  style:
+                                                      TextStyle(fontSize: 12),
+                                                )
+                                              ]),
+                                            ),
+                                            /////////////////////حق ريتريف الكرنت لوكيشن حق الحاج
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Column(children: [
+                                                IconButton(
+                                                  icon: Icon(
+                                                    (Icons.location_history_sharp),
+                                                    color: Color.fromARGB(255, 160, 10, 10),
+                                                    ),
+                                                    onPressed: () async {
+
+        //_latitude = storedocs[i]['latitude'];
+        //_longitude = storedocs[i]['longitude'];
+
+    getActivityProfile() async{
+  await _firestore
+  .collection('Pilgrims-Account')
+  .doc(storedocs[i]['UID']).get().then(
+    (doc)=>{
+      setState((){
+        if (doc.data() != null){
+          _latitude = doc.data()!['latitude']??=0;
+          _longitude = doc.data()!['longitude']??=0;
+        }
+      },
+      
+      ),
+    },
+      );
+      };
+    
+  // _firestore
+  // .collection('Pilgrims-Account')
+  // .doc(storedocs[i]['UID']).get().then(
+  //   (doc)=>{
+  //     setState((){
+  //       if (doc.data() != null){
+  //         _latitude = doc.data()!['latitude']??=0;
+  //         _longitude = doc.data()!['longitude']??=0;
+  //       }
+  //     },
+      
+  //     ),
+  //   },
+  //     );
+  LocationService _locationService = LocationService();
+  _locationService.goToMaps(_latitude, _longitude);
+    //////////////////////////////////////////////////
+                                                    },
+                                                    )
+                                              ]),
+                                            ),
+                                            //////////////////////////////// هنا نهايته
+                                          ]),
+                                        ]),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )));
+              },
+              ),
+              )
+              
+    ],
+    )
+    )
+
+    );
+    } //throw UnimplementedError();
+}
