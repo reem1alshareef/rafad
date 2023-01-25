e5439b00a9ac7e219f699edfcb0c218b9556f3b3
sadeem74 <sadeem.faisal@outlook.com>
2023-01-25 22:01:33 +0300

    s

--- lib/screens/ViewCampaignPlan.dart
+++ lib/screens/ViewCampaignPlan.dart
@@ -1,34 +1,60 @@
-import 'dart:html';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 
 class ViewCampaignPlan extends StatefulWidget {
   //static const String screenRoute = 'welcome_screen';
   const ViewCampaignPlan({super.key});
   @override
   _ViewState createState() => _ViewState();
+  String ID;
+
+  Future<void> start() async {
+    DocumentSnapshot variable = await FirebaseFirestore.instance
+          .collection('Pilgrims-Account')
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get();
+      ID = variable["ChosenCampaignID"];
+      print(ID);
+   // }
+
+   //getID();
+    //}
+
+    print(ID);
+    Stream<QuerySnapshot> dataStream = FirebaseFirestore.instance
+        .collection("AcceptedCampaigns")
+        .doc(ID)
+        .collection("Activities")
+        .snapshots();
+    print(ID);
+  }
 }
 
 class _ViewState extends State<ViewCampaignPlan> {
   @override
-  Widget build(BuildContext context) {
+  Future<Widget> build(BuildContext context) async {
+    String ID = 'blah';
 
-Future<DocumentSnapshot<Object?>> variable =  FirebaseFirestore.instance
-                                    .collection('Pilgrims-Account')
-                                    .doc(FirebaseAuth.instance.currentUser?.uid)
-                                    .get();
-                               
+    
 
-
+    print(ID);
     Stream<QuerySnapshot> dataStream = FirebaseFirestore.instance
         .collection("AcceptedCampaigns")
-        .doc('4a8v9veM6igptqvtBdq8cZvuczp2')
+        .doc(ID)
         .collection("Activities")
         .snapshots();
+    print(ID);
 
     return Scaffold(
+      DocumentSnapshot variable = await FirebaseFirestore.instance
+          .collection('Pilgrims-Account')
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get();
+      ID = variable["ChosenCampaignID"];
+      print(ID);
         appBar: AppBar(
           automaticallyImplyLeading: false,
           title: Text('Today\'s Campaign plan'),
@@ -65,30 +91,27 @@ Future<DocumentSnapshot<Object?>> variable =  FirebaseFirestore.instance
                         child: Container(
                           margin: EdgeInsets.only(bottom: 17),
                           child: ListTile(
-                            
                             title: Text(
                               storedocs[i]['title'],
-                              style: TextStyle(color: Color(0xFF455D83), fontSize: 20),
-                            ),
-                            trailing: Row( mainAxisSize: MainAxisSize.min, children:[ 
-                            Text(
-                              ' Starts at  '
-                              ' ${storedocs[i]['time'].toDate().hour} : ${storedocs[i]['time'].toDate().minute} ',
                               style: TextStyle(
-                                  color: Color(0xFF455D83),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17),
+                                  color: Color(0xFF455D83), fontSize: 20),
                             ),
-                            
-                             Icon(Icons.access_time_rounded)
-                            
-                            ] ),
+                            trailing:
+                                Row(mainAxisSize: MainAxisSize.min, children: [
+                              Text(
+                                ' Starts at  '
+                                ' ${storedocs[i]['time'].toDate().hour} : ${storedocs[i]['time'].toDate().minute} ',
+                                style: TextStyle(
+                                    color: Color(0xFF455D83),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                              ),
+                              Icon(Icons.access_time_rounded)
+                            ]),
                             tileColor: Color.fromARGB(255, 228, 232, 235),
                             contentPadding: EdgeInsets.all(17),
-                            
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                          
-             
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
                           ),
                         ),
                       ),
