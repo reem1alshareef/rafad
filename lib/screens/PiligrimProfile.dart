48be000c4698a15f8c267d24df63f2ecbba12381
Jihan-Almak <jalmakrami@gmail.com>
2023-01-26 10:40:40 +0300

    kkkk

--- lib/screens/PiligrimProfile.dart
+++ lib/screens/PiligrimProfile.dart
@@ -1,12 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:rafad1/screens/PiligrimProfile.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart'; // for 5 try
+import 'package:location/location.dart' as loc; // for 5 try
+import 'package:rafad1/screens/welcome_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+import 'navigation_screen.dart';
+
+/*import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rafad1/screens/welcome_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:rafad1/screens/navigation_screen.dart';
+//import 'package:rafad1/screens/navigation_screen.dart'; الكود السابق
 import 'package:permission_handler/permission_handler.dart';
 import 'package:location/location.dart' as loc;
 import 'package:rafad1/screens/ProfileCampaign.dart';
 import 'package:rafad1/screens/campaignRealMap.dart';
+import 'dart:async';*/
 
 class PiligrimProfile extends StatefulWidget {
   static const String screenRoute = 'PiligrimProfile.dart';
@@ -17,11 +28,8 @@ class PiligrimProfile extends StatefulWidget {
 }
 
 class _PiligrimProfileState extends State<PiligrimProfile> {
-  final _firestore = FirebaseFirestore.instance;
-
-  // final _controller = TextEditingController();
-  // double latController = (21.3878387); //ضبطت هذي الحركة
-  // double lngController = (39.9144660); //ضبطت هذي الحركة
+  double latController = (21.3878387); //ضبطت هذي الحركة
+  double lngController = (39.9144660); //ضبطت هذي الحركة
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,45 +51,18 @@ class _PiligrimProfileState extends State<PiligrimProfile> {
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Expanded(
-              child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('AcceptedCampaigns')
-                .snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return ListView.builder(
-                  itemCount: snapshot.data?.docs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      /* title:
-                          Text(snapshot.data!.docs[index]['name'].toString()),
-                      subtitle: Row(
-                        children: [
-                          Text(snapshot.data!.docs[index]['latitude']
-                              .toString()),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(snapshot.data!.docs[index]['longitude']
-                              .toString()),
-                        ],
-                      ),*/
-                      trailing: IconButton(
-                        icon: Icon(Icons.directions),
-                        onPressed: () {
-                          //هذا المكان اللي اذا ضغط عليه يوديه للموقع
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => campaignRealMap(
-                                  snapshot.data!.docs[index].id)));
-                        },
-                      ),
-                    );
-                  });
-            },
-          )),
+          // ممتاز هنا بجرب احضر اللوكيشن من الفايربيس وبيضبط ان شاء الله
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => NavigationScreen(
+                          latController, //double.parse(latController.text),
+                          lngController))); //double.parse(lngController.text))));
+                },
+                child: Text('Get Camapign Directions')),
+          ),
         ]),
       ),
     );
