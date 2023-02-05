67f4188497ad554befd6ef1c1584bf41ea16dff9
sadeem74 <sadeem.faisal@outlook.com>
2023-02-05 10:08:17 +0300

    Chat

--- lib/screens/Chat_screen.dart
+++ lib/screens/Chat_screen.dart
@@ -1,22 +1,107 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Chat_screen extends StatefulWidget {
-  const Chat_screen({super.key});
+  static const String screenRoute = 'chat_screen';
+
+  const Chat_screen({Key? key}) : super(key: key);
 
   @override
-  State<Chat_screen> createState() => _Chat_screenState();
+  _Chat_screenState createState() => _Chat_screenState();
 }
 
 class _Chat_screenState extends State<Chat_screen> {
+  final _auth = FirebaseAuth.instance;
+  late User signedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        signedInUser = user;
+        print(signedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: const Color(0xFF455D83),
-      title: Row(children: [Icon((Icons.messenger_outlined)), Text(' Chat'),],),
-      actions: [IconButton(onPressed: (){Navigator.of(
-                                                                      context)
-                                                                  .pop();}, icon: Icon((Icons.close)),)]));
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 12, 92, 119),
+        title: Row(
+          children: [
+            Image.asset('assests/images/chat.png' ,height: 35),
+            SizedBox(width: 10),
+            Text('Chat')
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut();
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.close),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Color.fromARGB(255, 41, 92, 116),
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        hintText: 'Write your message here...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'send',
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
