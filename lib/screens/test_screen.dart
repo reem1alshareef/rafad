d7d0dbd50ce5dc718dce7413214f5234f0402ee4
reem1alshareef <441200087@student.ksu.edu.sa>
2023-02-08 21:59:26 +0300

    و

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,36 +1,36 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
 
-class TestScreen extends StatelessWidget {
-  const TestScreen({super.key, this.docId});
-  final String? docId;
+// class TestScreen extends StatelessWidget {
+//   const TestScreen({super.key, this.docId});
+//   final String? docId;
 
-  // User user = FirebaseAuth.instance.currentUser!;
-  @override
-  Widget build(BuildContext context) {
-  CollectionReference users = FirebaseFirestore.instance.collection('Accespted-Bookings');
-    return Scaffold(
-      appBar: AppBar(),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: users.doc(docId).get(),
-        builder: ((context, snapshot) {
-          if (snapshot.hasError) {
-            return Text("Something went wrong");
-          }
-          if (snapshot.hasData && !snapshot.data!.exists) {
-            return const Text("Document does not exist");
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-            return Text("Name: ${data['name']} HajjID: ${data['HajjID']}");
-          }
-          return const Text('Loading');
-        }),
-      )
-    );
-  }
-}
+//   // User user = FirebaseAuth.instance.currentUser!;
+//   @override
+//   Widget build(BuildContext context) {
+//   CollectionReference users = FirebaseFirestore.instance.collection('Accespted-Bookings');
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: FutureBuilder<DocumentSnapshot>(
+//         future: users.doc(docId).get(),
+//         builder: ((context, snapshot) {
+//           if (snapshot.hasError) {
+//             return Text("Something went wrong");
+//           }
+//           if (snapshot.hasData && !snapshot.data!.exists) {
+//             return const Text("Document does not exist");
+//           }
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+//             return Text("Name: ${data['name']} HajjID: ${data['HajjID']}");
+//           }
+//           return const Text('Loading');
+//         }),
+//       )
+//     );
+//   }
+// }
 
 // StreamBuilder(
 //         stream:  FirebaseFirestore.instance.collection('Accepted-Bookings').snapshots(),
